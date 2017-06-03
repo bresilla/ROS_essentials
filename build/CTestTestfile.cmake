e2277a4bdf1b5b1c85b35d100eef2223bedd1f9e
Trim Bresilla <trim.bresilla@gmail.com>
2017-06-03 16:25:20 +0200

    first catking make

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/trim/CODE/ROBO/part_2/ros_essentials/src
+# Build directory: /home/trim/CODE/ROBO/part_2/ros_essentials/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/trim/CODE/ROBO/part_2/ros_essentials/devel:$CMAKE_PREFIX_PATH"
+export PATH="/opt/ros/kinetic/bin:/home/trim/.cargo/bin:/home/trim/bin:/home/trim/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PWD="/home/trim/CODE/ROBO/part_2/ros_essentials/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/trim/CODE/ROBO/part_2/ros_essentials/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/trim/CODE/ROBO/part_2/ros_essentials/src:$ROS_PACKAGE_PATH"
