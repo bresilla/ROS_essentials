35330624b88d7a92e6f650e70640ae8f7e1d88bc
Trim Bresilla <trim.bresilla@gmail.com>
2017-06-03 02:51:38 +0200

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/trim/CODE/ROBO/part_2/ROS_essentials/root_ws/src
+# Build directory: /home/trim/CODE/ROBO/part_2/ROS_essentials/root_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
