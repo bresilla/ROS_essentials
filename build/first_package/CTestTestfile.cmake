037c66087047c382bb0a6e642b35e59de4d371ae
Trim Bresilla <trim.bresilla@gmail.com>
2017-06-03 18:51:24 +0200

    first_package

---
+++ build/first_package/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/trim/CODE/ROBO/part_2/ros_essentials/src/first_package
+# Build directory: /home/trim/CODE/ROBO/part_2/ros_essentials/build/first_package
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
